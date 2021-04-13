e2b8523dcd70bbe9ab02fe0d79256a0325f23280
lisswart <arnoudlisa@gmail.com>
2021-04-13 01:59:56 -0400

    revised identifier names

--- js/index.js
+++ js/index.js
@@ -5,59 +5,64 @@ document.addEventListener("DOMContentLoaded", function() {
 function fetchBooks() {
     return fetch("http://localhost:3000/books")
     .then(resp => resp.json())
-    .then(json => renderBooks(json));
+    .then(json => bookTitles(json));
 }
 
-function renderBooks(books) {
+function bookTitles(books) {
     books.forEach(book => {
-        loadImage(book);
-        getAuthor(book);
         getBookTitle(book);
+        loadImage(book);
+        addLikeButton();
+        getAuthor(book);
         getBookSubtitle(book);
         getBookDescription(book);
-        const bulletBook = document.querySelector('#list');
-        const bookDiv = document.createElement('div');
-        bulletBook.appendChild(bookDiv);
     });
 }
 
 function getBookTitle(book) {
     const unorderedList = document.querySelector('#list');
     const list = document.createElement('li');
-    list.innerHTML = `<a href="#"><h3><i>${book.title}</i></h3></a>`;
+    list.innerHTML = `<a href='#'><h3><i>${book.title}</i></h3></a>`;
     unorderedList.appendChild(list);
 }
 
 function getBookSubtitle(book) {
-    const unorderedList = document.querySelector('#show-panel');
-    const list = document.createElement('h4');
-    list.textContent = `${book.subtitle}`;
-    unorderedList.appendChild(list);
+    const showPanel = document.querySelector('#show-panel');
+    const subTitle = document.createElement('h4');
+    subTitle.className = "hidden";    
+    subTitle.textContent = `${book.subtitle}`;
+    showPanel.appendChild(subTitle);
 }
 
-function getBookDescription(book) {
-    const unorderedList = document.querySelector('#show-panel');
-    const list = document.createElement('p');
-    list.textContent = `${book.description}`;
-    list.className = "description";
-    unorderedList.appendChild(list);
+function getBookDescription(book) {   
+    const showPanel = document.querySelector('#show-panel');
+    const description = document.createElement('p');
+    description.className = "hidden description";    
+    description.textContent = `${book.description}`;
+    showPanel.appendChild(description);
 }
 
 function loadImage(book) {
-    const unorderedList = document.querySelector('#show-panel');
-    const imgDiv = document.createElement('div');
+    const showPanel = document.querySelector('#show-panel');
     const imgBook = document.createElement('img');
+    imgBook.className = "hidden image";
     imgBook.src = book.img_url;
-    unorderedList.appendChild(imgDiv);
-    imgDiv.appendChild(imgBook);
-    imgDiv.style.display = 'flex';
-    imgDiv.style.justifyContent = 'left';
+    showPanel.appendChild(imgBook);
 }
 
-function getAuthor(book) {
-    const unorderedList = document.querySelector('#show-panel');
-    const list = document.createElement('h2');
-    list.textContent = `${book.author}`;
-    list.className = "author";
-    unorderedList.appendChild(list);
+function getAuthor(book) {   
+    const showPanel = document.querySelector('#show-panel'); 
+    const author = document.createElement('h2');
+    author.className = "hidden author";
+    author.textContent = `${book.author}`;
+    showPanel.appendChild(author);
+}
+
+function addLikeButton() {
+    const showPanel = document.querySelector('#show-panel');
+    const like = document.createElement('button');
+    like.value = "like";
+    like.name = 'like';
+    like.textContent = "LIKE";
+    showPanel.appendChild(like);
 }
